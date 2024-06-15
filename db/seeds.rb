321b05524ae3d14b246c4dad3df5dbbeff4e9f41
varunabm <varunabayu.2021@student.uny.ac.id>
2024-06-15 07:41:41 +0000

    menambahkan isi users dan isi articles

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-data = {full_name: 'Budi Gunawan', email: 'budigunawan@gmail.com', password: '1234abc'}
+data = {full_name: 'Ilmining Mandiri', email: 'ilmimandiri@gmail.com', password: 'iniilmi'}
 user = User.create(data)
 
 user = User.first
