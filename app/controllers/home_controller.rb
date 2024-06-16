c9e4ed9ef8c748b3ffa701f98394adbdf050df47
varunabm <varunabayu.2021@student.uny.ac.id>
2024-06-16 06:35:30 +0000

    membuat navigasi

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,14 +7,22 @@ class HomeController < ApplicationController
     @article = Article.where(id: params[:article_id]).first
   end
 
-  def save_comment
-    @article = Article.where(id: params[:article_id]).first
-    comment =  @article.comments.new
+  def save_comment 
+    @article = Article.where(id: params[article_id]).first
+    comment = @article.comments.new
     comment.comment = params[:comment]
     if comment.save
-      redirect_to read_path(article_id: @article.id, notice: 'Succes')
+      redirect_to read_path(article_id: @article.id, notice: 'Success')
     else
       redirect_to read_path(article_id: @article.id, notice: 'Error')
     end
+  end
+
+  def about
+  end
+
+  def search
+    keyword = params[:keyword]
+    @articles = Article.where("title LIKE '%#{keyword}%'")
   end
 end
